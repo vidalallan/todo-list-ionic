1b61981c4b0bad0d93598d2525fa3a56f6651eef
Allan Vidal <allan@tew.one>
2025-12-08 09:08:41 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'todolist',
+  webDir: 'www'
+};
+
+export default config;
